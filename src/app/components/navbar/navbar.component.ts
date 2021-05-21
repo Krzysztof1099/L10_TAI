fed4a8f53c6179f4dead243cb5bd8e063eeb4f77
Krzysztof Piechnik <krzysztofpiechnik1999@interia.pl>
2021-05-21 19:20:51 +0200

    Laboratorium 10 - Wykonano wszystkie zadania

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
 @Component({
-  selector: 'navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+selector: 'navbar',
+templateUrl: './navbar.component.html',
+styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+constructor(public authService: AuthService, public router: Router) {
+}
+ngOnInit(): void {
+}
+signOut() {
+this.authService.logout().subscribe((result: any) => {this.router.navigate(['/']);
+return result;
+});
+}
 }
